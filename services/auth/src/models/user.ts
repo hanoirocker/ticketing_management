9348496e35d5a15d122e23e158c15c82953fb877
hanoirocker <Rafael.Aguero@hearst.com>
2025-03-21 10:48:19 -0300

    Auth: Define user model and adapt to typescript

--- services/auth/src/models/user.ts
+++ services/auth/src/models/user.ts
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose';
 
-// Define schema for user. This is only for mongoose.
-// This file doesn't help TS to know which properties we need to pass when
-// creating a new user at all.
+interface UserAttrs {
+  email: string;
+  password: string;
+}
 
+/**
+ * Define schema for user. This is only for mongoose.
+ * This doesn't help TS to know which properties we need to pass when
+ * creating a new user at all. For addresing this, we'll use
+ * `buildUser` function and `UserAttrs` interface.
+ */
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -17,4 +24,13 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-export { User };
+/**
+ * Specific function for building a user instead of calling `new User(...)` directly.
+ * This is for integrating mongoose with typescrit to correctly define
+ * attributes needed by using the UserAttrs TS interface.
+ */
+const buildUser = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+export { User, buildUser };
