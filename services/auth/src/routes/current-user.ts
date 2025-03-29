9b77f8f36b705e2cc29a1d463a2760777f797758
hanoirocker <Rafael.Aguero@hearst.com>
2025-03-29 16:05:11 -0300

    Auth: return currentUser and log out

--- services/auth/src/routes/current-user.ts
+++ services/auth/src/routes/current-user.ts
@@ -1,9 +1,24 @@
+import { captureRejectionSymbol } from 'events';
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
+// This route will be used to get the current user JWT if exists,
+// for our React app to verify if the user is logged in or not
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('current user!');
+  console.log('Getting current user!');
+
+  if (!req.session?.jwt) {
+    res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
--- services/auth/src/routes/signout.ts
+++ services/auth/src/routes/signout.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cookieSession from 'cookie-session';
 
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('sign out!');
+  console.log('Trying to sign out!');
+
+  req.session = null; // This will remove the JWT token from the session
+  res.send({}); // Send an empty response
 });
 
 export { router as signoutRouter };
