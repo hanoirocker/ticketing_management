87a99de30f7691d4f47280c66997ff44da36bb94
hanoirocker <Rafael.Aguero@hearst.com>
2025-04-26 14:24:22 -0300

    Auth: tests_ refactor index.js and create app.js

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,40 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true); // trust proxy (since we're using ingress-nginx)
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false, // don't encrypt the cookie
-    secure: true, // only use cookies on https (not needed for dev)
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// If any not defined route is trying to be called, we raise an specific Error for it.
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-// Initialize errorHandler middleware. Basically, if any 'throw new <error_class>' is
-// used on any of the request files, the middleware will processed this error classes and
-// return a structured and solid message to the client (React app)
-app.use(errorHandler);
+import { app } from './app';
 
 /**
  * Connect to MongoDB cluster ip using mongoose and start listeting on app port after it.
