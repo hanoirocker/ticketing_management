dccd60841dcfe5602b4cb14f3d418fc126bc6d13
hanoirocker <Rafael.Aguero@hearst.com>
2025-04-26 23:50:51 -0300

    Client: create useRequests hook

---
+++ services/client/hooks/use-requests.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function useRequest({ url, method, body }) {
+  const [errors, setErrors] = useState(null);
+
+  const doRequests = async () => {
+    try {
+      const res = await axios[method](url, body);
+      return res.data;
+    } catch (err) {
+      setErrors(<div className="alert alert-danger">
+        <h4>Damn!</h4>
+        <ul className="my-0">
+          {err.response.data.errors.map(err => <li key={err.message}>{err.message}</li>)}
+        </ul>
+      </div>);
+    };
+  };
+
+  return { doRequests, errors }
+};
--- services/client/pages/auth/signup.js
+++ services/client/pages/auth/signup.js
@@ -1,21 +1,19 @@
 import { useState } from 'react';
-import axios from 'axios';
+import useRequest from '../../hooks/use-requests';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setError] = useState([]);
+  const { doRequests, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: { email, password },
+  });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    try {
-      const res = await axios.post('/api/users/signup', {
-        email, password
-      })
-    } catch (err) {
-      setError(err.response.data.errors);
-    }
+    doRequests();
   };
 
   return (
@@ -29,12 +27,7 @@ export default () => {
         <label>Password</label>
         <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="form-control" />
       </div>
-      {errors.length > 0 && (<div className="alert alert-danger">
-        <h4>Damn!</h4>
-        <ul className="my-0">
-          {errors.map(err => <li key={err.message}>{err.message}</li>)}
-        </ul>
-      </div>)}
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
