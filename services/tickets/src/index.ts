5b5c3dd99fad04339f938e50a836884534f0235d
hanoirocker <Rafael.Aguero@hearst.com>
2025-05-17 00:34:11 -0300

    Tickets: update infra depls
    Tickets: update infra and index.ts
    
    Tickets: update infra and index.ts

--- services/tickets/src/index.ts
+++ services/tickets/src/index.ts
@@ -8,9 +8,9 @@ import { app } from './app';
  * Connect to MongoDB cluster ip using mongoose and start listeting on app port after it.
  *
  * 'mongodb' - needed when using mongoose
- * 'auth-mongo-srv' - metadata/name of the service to connect to.
+ * 'tickets-mongo-srv' - metadata/name of the service to connect to.
  * ':27017' - cluster ip defined port.
- * 'auth' - name for the database to create (could be anything actually)
+ * 'tickets' - name for the database to create (could be anything actually)
  */
 const start = async () => {
   // signing key for validating the token. Retrieved from env variables as secret key
@@ -18,15 +18,20 @@ const start = async () => {
     throw new Error('JWT_KEY must be defined');
   }
 
+  // check if mongodb URI is defined at infra depl file
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log('Auth service Listening on port 3000');
+    console.log('tickets service Listening on port 3000');
   });
 };
 
