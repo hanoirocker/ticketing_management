f5a96b758f8087b3fd18f43216855f6c4d68664f
hanoirocker <Rafael.Aguero@hearst.com>
2025-05-17 12:34:29 -0300

    Tickets: create invalid params test

--- services/tickets/src/app.ts
+++ services/tickets/src/app.ts
@@ -7,6 +7,7 @@ import {
   NotFoundError,
   currentUser,
   requireAuth,
+  validateRequest,
 } from '@hanoiorg/ticketing_common';
 import { createTicketRouter } from './routes/new';
 
@@ -21,6 +22,7 @@ app.use(
 );
 app.use(currentUser);
 app.use(requireAuth);
+app.use(validateRequest);
 app.use(createTicketRouter);
 
 // If any not defined route is trying to be called, we raise an specific Error for it.
--- services/tickets/src/routes/__test__/new.test.ts
+++ services/tickets/src/routes/__test__/new.test.ts
@@ -25,8 +25,47 @@ it('returns a status other than 401 if the user is signed in', async () => {
   expect(res.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+  // validate-request middleware defines RequestValidationError for requests
+  // missing params (status 400). Let's use it.
 
-it('returns an error if an invalid price is provided', async () => {});
+  // Case: user misses title param or is empty.
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: '10',
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: '10',
+    })
+    .expect(400);
+});
+
+it('returns an error if an invalid price is provided', async () => {
+  // Case: user misses price param or is invalid.
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'test',
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'test',
+    })
+    .expect(400);
+});
 
 it('creates a ticket with valid inputs', async () => {});
