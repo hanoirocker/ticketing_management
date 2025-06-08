3acecbd14a9f2322124988ff92f3a0a0d54a6f6f
hanoirocker <Rafael.Aguero@hearst.com>
2025-06-08 19:16:19 -0300

    NATSss: prevent NATS waiting for shutdowned services

--- services/nats-test/src/listener.ts
+++ services/nats-test/src/listener.ts
@@ -12,6 +12,14 @@ const stan = nats.connect('ticketing', clientId, {
 stan.on('connect', () => {
   console.log(`Listener with clientId ${clientId} connencted to NATS SS`);
 
+  // Watches for close events from NATS, to be receieved after any interrumption or
+  // termination signal is intercepted
+  stan.on('close', () => {
+    console.log('NATS connection closed');
+    // Kill the program if client is down
+    process.exit();
+  });
+
   // To set options, we need to call each method as chain after `subscriptionOptions`
   // setManualAckMode(true): stops the default 'everything is ok onced msg is received' behaviour.
   // This way, we can process msg and let NATS know everthing is fine once we sucessfuly
@@ -36,3 +44,8 @@ stan.on('connect', () => {
     msg.ack();
   });
 });
+
+// Watches for interrupt or termination signals. If any of those are intercepted,
+// we tell NATS to not send any more messages into this client
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
