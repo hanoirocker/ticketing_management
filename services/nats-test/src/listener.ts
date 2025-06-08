2f3fffb005d384534f8d57d1660c39c75fa962be
hanoirocker <Rafael.Aguero@hearst.com>
2025-06-08 18:52:12 -0300

    NATSss: set ack manual mode

--- services/nats-test/src/listener.ts
+++ services/nats-test/src/listener.ts
@@ -12,9 +12,17 @@ const stan = nats.connect('ticketing', clientId, {
 stan.on('connect', () => {
   console.log(`Listener with clientId ${clientId} connencted to NATS SS`);
 
+  // To set options, we need to call each method as chain after `subscriptionOptions`
+  // setManualAckMode(true): stops the default 'everything is ok onced msg is received' behaviour.
+  // This way, we can process msg and let NATS know everthing is fine once we sucessfuly
+  // made what we wanted with it. If after 30 secs NATS hasn't received any confirmation,
+  // the msg will be sent to any other member of the queue group
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  // Set up channel for listening to and queue name
   const subscription = stan.subscribe(
     'ticket:created',
-    'orders-service-queue-group'
+    'orders-service-queue-group',
+    options
   );
 
   subscription.on('message', (msg: Message) => {
@@ -23,5 +31,8 @@ stan.on('connect', () => {
     if (typeof data === 'string') {
       console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    // Let's NATS know event has been processed correctly.
+    msg.ack();
   });
 });
