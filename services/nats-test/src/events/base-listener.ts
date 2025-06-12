8b35fdff0a62ebac70744498466cd104d34d97b3
hanoirocker <Rafael.Aguero@hearst.com>
2025-06-12 20:58:34 -0300

    NATSss: refactor classes and listener.ts

---
+++ services/nats-test/src/events/base-listener.ts
@@ -0,0 +1,60 @@
+import { Message, Stan } from 'node-nats-streaming';
+
+// Abstract class to instace a listener for all different kind of events
+// flowing throgh our app
+export abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void; // Where business logic lives
+
+  private client: Stan;
+  protected ackWait = 5 * 1000; // time to wait for an event, 5 secs
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  /**
+   * To set options, we need to call each method as chain after `subscriptionOptions`
+   *
+   * - setMnualAckMode(true): stops the default 'everything is ok onced msg is received' behaviour.
+   * Thisway, we can process msg and let NATS know everthing is fine once we sucessfuly
+   * madewhat we wanted with it. If after 30 secs NATS hasn't received any confirmation,
+   * the msg will be sent to any other member of the queue group
+   * - setDeliverAllAvailable(): makes it possible for retreving all events processed in the past
+   * in case the service goes down. This is run only one time as the services goes up.
+   * - setDurableName(): makes it possible to return only not processed sucesfully events,
+   * so we don't retrieve ALL events after a service goes down.
+   */
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  // Set up channel for listening to and queue name
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message recieved: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data) // parse string
+      : JSON.parse(data.toString('utf8')); // parse a buffer
+  }
+}
--- services/nats-test/src/listener.ts
+++ services/nats-test/src/listener.ts
@@ -1,4 +1,5 @@
-import nats, { Message, Stan } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 import { randomBytes } from 'crypto';
 
 console.clear();
@@ -30,73 +31,3 @@ stan.on('connect', () => {
 // we tell NATS to not send any more messages into this client
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
-
-// Abstract class to instace a listener for all different kind of events
-// flowing throgh our app
-abstract class Listener {
-  abstract subject: string;
-  abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void; // Where business logic lives
-
-  private client: Stan;
-  protected ackWait = 5 * 1000; // time to wait for an event, 5 secs
-
-  constructor(client: Stan) {
-    this.client = client;
-  }
-
-  /**
-   * To set options, we need to call each method as chain after `subscriptionOptions`
-   *
-   * - setMnualAckMode(true): stops the default 'everything is ok onced msg is received' behaviour.
-   * Thisway, we can process msg and let NATS know everthing is fine once we sucessfuly
-   * madewhat we wanted with it. If after 30 secs NATS hasn't received any confirmation,
-   * the msg will be sent to any other member of the queue group
-   * - setDeliverAllAvailable(): makes it possible for retreving all events processed in the past
-   * in case the service goes down. This is run only one time as the services goes up.
-   * - setDurableName(): makes it possible to return only not processed sucesfully events,
-   * so we don't retrieve ALL events after a service goes down.
-   */
-  subscriptionOptions() {
-    return this.client
-      .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName);
-  }
-
-  // Set up channel for listening to and queue name
-  listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    );
-
-    subscription.on('message', (msg: Message) => {
-      console.log(`Message recieved: ${this.subject} / ${this.queueGroupName}`);
-
-      const parsedData = this.parseMessage(msg);
-      this.onMessage(parsedData, msg);
-    });
-  }
-
-  parseMessage(msg: Message) {
-    const data = msg.getData();
-    return typeof data === 'string'
-      ? JSON.parse(data) // parse string
-      : JSON.parse(data.toString('utf8')); // parse a buffer
-  }
-}
-
-class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
-  queueGroupName = 'payment-service';
-  onMessage(data: any, msg: Message) {
-    console.log('Event data:', data);
-
-    // Marks the message as sucessfully being parsed
-    msg.ack();
-  }
-}
