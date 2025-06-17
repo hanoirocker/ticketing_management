1a8786688da89b5d09ebb7f6da20a68bc2ac5f44
hanoirocker <Rafael.Aguero@hearst.com>
2025-06-17 20:48:43 -0300

    Orders: create first validations for new.ts route

--- services/orders/src/routes/new.ts
+++ services/orders/src/routes/new.ts
@@ -1,5 +1,11 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@hanoiorg/ticketing_common';
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@hanoiorg/ticketing_common';
 import mongoose from 'mongoose';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
@@ -7,7 +13,7 @@ import { Order } from '../models/order';
 
 const router = express.Router();
 
-// If in the future the ticketId structure changes from within the tickets service
+// If in the future the ticket id structure changes from within the tickets service
 // we would have to modify/delete .custom validation. Right now is 100% based on mongoose's structure
 router.post(
   '/api/orders',
@@ -21,9 +27,33 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
     // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
 
     // Make sure this ticket is not already reserved
+    // Run query to look at all orders. Find an order where the ticket is
+    // the ticket we just found AND the orders status IS NOT cancelled.
+    // if we find an order from that, it means the ticket IS reserved.
+    const alreadyExistingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+        ],
+      },
+    });
+
+    if (alreadyExistingOrder) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
 
     // Calculate an expiration date for this order
 
