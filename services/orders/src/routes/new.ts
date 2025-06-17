2b3a50b47825d8ac545b66074adc4c450e4968c8
hanoirocker <Rafael.Aguero@hearst.com>
2025-06-17 20:37:33 -0300

    Orders: some pseudo code to guide me on new.ts route

--- services/orders/src/routes/new.ts
+++ services/orders/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { requireAuth, validateRequest } from '@hanoiorg/ticketing_common';
 import mongoose from 'mongoose';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -19,6 +21,15 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    // Find the ticket the user is trying to order in the database
+
+    // Make sure this ticket is not already reserved
+
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish order:created event
     res.send({});
   }
 );
