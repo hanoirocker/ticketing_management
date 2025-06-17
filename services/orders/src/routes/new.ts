f37dd519af1787425afa6b3e4ddfc03e9b5f245d
hanoirocker <Rafael.Aguero@hearst.com>
2025-06-17 19:10:33 -0300

    Orders: update new.ts route

--- services/orders/src/routes/new.ts
+++ services/orders/src/routes/new.ts
@@ -1,9 +1,26 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@hanoiorg/ticketing_common';
+import mongoose from 'mongoose';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+// If in the future the ticketId structure changes from within the tickets service
+// we would have to modify/delete .custom validation. Right now is 100% based on mongoose's structure
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided!'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
