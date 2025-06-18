20114c2f09ee55a628c70c4b0988da6a63e5959b
hanoirocker <Rafael.Aguero@hearst.com>
2025-06-18 00:48:28 -0300

    Orders: work around index.ts file to get orders for user

--- services/orders/src/routes/index.ts
+++ services/orders/src/routes/index.ts
@@ -1,9 +1,15 @@
+import { requireAuth } from '@hanoiorg/ticketing_common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser?.id,
+  }).populate('ticket'); // mongoose 'populate' method adds related ticket into each order retrieved
+
+  res.send({ orders });
 });
 
 export { router as indexOrderRouter };
