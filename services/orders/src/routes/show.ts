d21f930ebb5a2818cb12f4e418495869b8e9d4a9
hanoirocker <Rafael.Aguero@hearst.com>
2025-06-18 01:27:43 -0300

    Orders: work on show.ts route for getting specific order by id

--- services/orders/src/routes/show.ts
+++ services/orders/src/routes/show.ts
@@ -1,9 +1,39 @@
 import express, { Request, Response } from 'express';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@hanoiorg/ticketing_common';
+import { Order } from '../models/order';
+import { param } from 'express-validator';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  '/api/orders/:orderId',
+  requireAuth,
+  [
+    param('orderId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('orderId must be provided and valid!'),
+  ],
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+
+    // If order is not found for given id
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    // for preventing a user having access to other user's orders data
+    if (order.userId !== req.currentUser?.id) {
+      throw new NotAuthorizedError();
+    }
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
