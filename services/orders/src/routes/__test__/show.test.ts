08dc43eb50aa8a76a354d93d8f4706b11683014b
hanoirocker <Rafael.Aguero@hearst.com>
2025-06-18 01:38:25 -0300

    Orders: add show tests

---
+++ services/orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,56 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+it('fetches the order', async () => {
+  const user = global.signin();
+
+  // First create the ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 10,
+  });
+  await ticket.save();
+
+  // Then make the request to build the order of the previous ticket
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Now try to fetch it
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+
+  expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it('returns an error if one user tries to fetch another user order', async () => {
+  const user = global.signin();
+  const user2 = global.signin();
+
+  // First create the ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 10,
+  });
+  await ticket.save();
+
+  // Then make the request to build the order of the previous ticket
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Now try to fetch it by using user2 (the hacker)
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user2)
+    .send()
+    .expect(401);
+});
