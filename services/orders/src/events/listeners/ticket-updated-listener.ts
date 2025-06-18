9df90cc9d60d96120b64917e527ab94e85f71efe
hanoirocker <Rafael.Aguero@hearst.com>
2025-06-18 17:39:22 -0300

    Orders: create TicketUpdatedListener

---
+++ services/orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,27 @@
+import { Message } from 'node-nats-streaming';
+import {
+  Subjects,
+  Listener,
+  TicketUpdatedEvent,
+} from '@hanoiorg/ticketing_common';
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.id); // try to find the saved ticket by its id
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    const { title, price } = data;
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
