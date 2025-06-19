705d9bc0cbfc44cd2ba1203d7e0accba2e10c674
hanoirocker <Rafael.Aguero@hearst.com>
2025-06-19 20:32:21 -0300

    Orders: plan listeners tests

---
+++ services/orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,14 @@
+const setup = async () => {
+  // Create an instance of the listener
+  // Create a fake data event
+  // Create a fake msg object
+};
+
+it('creates and saves a ticket', async () => {
+  // Call the onMessage function with data + msg objs
+  // Assertions on ticket creationg
+});
+it('acks the message', async () => {
+  // Call the onMessage function with data + msg objs
+  // Write assertions to make sure ack is called
+});
