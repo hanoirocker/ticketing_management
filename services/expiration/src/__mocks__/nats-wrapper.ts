cf067169b75932314301887fb83f83207710fb69
hanoirocker <Rafael.Aguero@hearst.com>
2025-07-04 13:49:03 -0300

    Expiration: copy initial files from tickets service

---
+++ services/expiration/src/__mocks__/nats-wrapper.ts
@@ -0,0 +1,20 @@
+/**
+ * Fake the real natsWrapper file for Jest to mock it on tests. For this, we need
+ * to understand what we expect from the nats client. So far, we can see that the only class
+ * making use of it is Publisher, so if we take a look into base-publisher code we'll see that
+ * it expects the client for publishing an event later on.
+ * So to sum it up, we'll need to mock the client and the publish function, along with
+ * data and callback as well.
+ */
+
+export const natsWrapper = {
+  client: {
+    publish: jest
+      .fn()
+      .mockImplementation(
+        (subject: string, data: string, callback: () => void) => {
+          callback();
+        }
+      ),
+  },
+};
