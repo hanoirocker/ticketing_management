43ce56569d87f00fed009f293af710eb96bc9211
hanoirocker <Rafael.Aguero@hearst.com>
2025-07-03 22:04:51 -0300

    Tickets: build order-cancelled-listener callback

--- services/tickets/src/events/listeners/order-cancelled-listener.ts
+++ services/tickets/src/events/listeners/order-cancelled-listener.ts
@@ -5,9 +5,37 @@ import {
 } from '@hanoiorg/ticketing_common';
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
   subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
   queueGroupName = queueGroupName;
-  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found!');
+    }
+
+    // NOTE:
+    // When this event is intercepted, we delete the orderId value from the ticket
+    // This way we mark the ticket as 'available' once again
+    ticket.set({ orderId: undefined });
+
+    await ticket.save();
+
+    // Emit a ticket updated event to let all services this ticket has updated his
+    // orderId and version values
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+    });
+
+    msg.ack();
+  }
 }
