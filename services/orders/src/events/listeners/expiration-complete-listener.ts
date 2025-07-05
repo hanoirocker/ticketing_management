15527978ce9551eff0e137fb2b38d9e969ff3c79
hanoirocker <Rafael.Aguero@hearst.com>
2025-07-04 23:08:17 -0300

    Orders: publish order:cancelled event on expiration-complete-listener

--- services/orders/src/events/listeners/expiration-complete-listener.ts
+++ services/orders/src/events/listeners/expiration-complete-listener.ts
@@ -7,12 +7,14 @@ import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
 import { Order } from '../../models/order';
 import { OrderStatus } from '@hanoiorg/ticketing_common';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+import { natsWrapper } from '../../nats-wrapper';
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
   queueGroupName = queueGroupName;
   async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId).populate('ticket');
 
     if (!order) {
       throw new Error('Order not found');
@@ -22,5 +24,18 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     order.set({
       status: OrderStatus.Cancelled,
     });
+
+    await order.save();
+
+    // Publish order:cancelled event to inform other services
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
   }
 }
