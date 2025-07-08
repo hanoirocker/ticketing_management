bd3f765510adb2b1f9b266efe6552c35338634d4
hanoirocker <Rafael.Aguero@hearst.com>
2025-07-08 15:34:55 -0300

    Payments: update-if-current to set version attr

---
+++ services/payments/src/models/order.ts
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@hanoiorg/ticketing_common';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+// List of properties we have to provide when building an order
+interface OrderAttrs {
+  id: string;
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+// List of properties that an order has once created
+interface OrderDoc extends mongoose.Document {
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+// List of properties that the model itself contains
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        // @ts-ignore TODO: revisit why this two lines present errors on id property
+        ret.id = ret._id;
+        // @ts-ignore
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
